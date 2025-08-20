8565db371d25d9dfa8acaabb7e89678fa1418d14
Lucas Fidelis <lucas.fidelis@viannasempre.com.br>
2025-08-20 17:03:10 -0300

    projetct created

---
+++ dblist/src/main/java/com/dev/prosolution/dblist/DblistApplication.java
@@ -0,0 +1,13 @@
+package com.dev.prosolution.dblist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DblistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DblistApplication.class, args);
+	}
+
+}
---
+++ dblist/src/test/java/com/dev/prosolution/dblist/DblistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.prosolution.dblist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DblistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
